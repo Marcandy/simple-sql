aa6ae29cb24c3754c7cd3409a59fcb676c448978
marcandy <marcandy.nj@gmail.com>
2017-01-09 14:26:40 -0700

    sql

--- track.js
+++ track.js
@@ -10,5 +10,10 @@ VALUES
  ('Boy', 25, 7.7, 'Provo', 'Green'),
  ('Coy', 26, 5.5, 'Provo', 'Yellow'),
  ('Boom', 27, 4.7, 'Provo', 'Purple');
+ ('Zeus', 18, 5.7, 'Dallas', 'Blue'),
+ ('Bob', 19, 6.7, 'Austin', 'Black')
 
 SELECT * FROM Person
+ORDER BY height desc
+ORDER BY age desc
+WHERE age > 20
