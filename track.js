ce8733ec6ff45972ea99446a072b67b7a6131052
marcandy <marcandy.nj@gmail.com>
2017-01-09 14:41:31 -0700

    sql

--- track.js
+++ track.js
@@ -11,9 +11,21 @@ VALUES
  ('Coy', 26, 5.5, 'Provo', 'Yellow'),
  ('Boom', 27, 4.7, 'Provo', 'Purple');
  ('Zeus', 18, 5.7, 'Dallas', 'Blue'),
- ('Bob', 19, 6.7, 'Austin', 'Black')
+ ('Bob', 19, 6.7, 'Austin', 'Black'),
+ ('Bob2', 39, 6.7, 'Austin', 'Black'),
+ ('Tor', 30, 6, 'Der', 'red'),
+ ('Bem', 35, 6.1, 'New York', 'orange')
 
 SELECT * FROM Person
+
 ORDER BY height desc
 ORDER BY age desc
 WHERE age > 20
+WHERE age = 18
+WHERE age < 20 or age > 30
+WHERE age != 27
+WHERE  FavoriteColor != 'red'
+WHERE  FavoriteColor != 'red' and FavoriteColor !=  'Blue'
+WHERE  FavoriteColor = 'orange' or FavoriteColor = 'Green'
+WHERE  FavoriteColor in ('orange', 'Green')
+WHERE  FavoriteColor in ('Yellow', 'Purple')
